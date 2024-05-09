09f3544b10daf4bc349c38d6097e0400fc64d9cb
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-09 15:47:55 -0300

    feat: reformater and refactor

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import "./components/nova-transacao-component";
+import "./components/saldo-component";
+import "./types/FormatoData";
+import "./types/TipoTransacao";
+import "./types/Transacao";
+import "./utils/formatters";
